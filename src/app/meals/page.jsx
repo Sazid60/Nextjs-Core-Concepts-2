6b0ecc77815c3e9821e95aff9606f9298ef0475d
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-09-15 13:40:00 +0600

    Exploring Custom Css Styling In Next.js

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,12 +1,13 @@
 
 import Meals from '@/components/Meals';
 import React from 'react';
+import styles from './styles.module.css'
 
 const MealsPage = () => {
     return (
         <div className='p-12'>
-            <h1 className='text-4xl font bold'>Choose Your Meals</h1>
-            <p>Chose Your Meals By Searching</p>
+            <h1 className={`text-4xl font-bold ${styles.font_tomato} ${styles.text_large}`}>Choose Your Meals</h1>
+            <p className={styles.font_tomato}>Chose Your Meals By Searching</p>
             <Meals />
         </div>
     );
